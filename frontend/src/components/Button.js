9f39bf00c48305c155784311b953a318f45842ea
RawiriHohepa <54158903+RawiriHohepa@users.noreply.github.com>
2021-07-16 12:21:26 +1200

    Added onClick function to toggle Button press

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -3,11 +3,15 @@ import { Button as MuiButton } from "@material-ui/core";
 
 export default function Button() {
     const [isPressed, setIsPressed] = useState(false);
+    function onClick() {
+        setIsPressed(!isPressed);
+    }
 
     return (
         <MuiButton
             variant={isPressed ? "outlined" : "contained"}
             color={"primary"}
+            onClick={onClick}
         >
             {isPressed ? "Unpress me!" : "Press me!"}
         </MuiButton>
