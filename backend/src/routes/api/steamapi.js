0638e5b3c72cd3052962c01c48026b776a498043
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-07-31 15:46:44 +1200

    adding steamapi functions

--- backend/src/routes/api/steamapi.js
+++ backend/src/routes/api/steamapi.js
@@ -1,6 +1,43 @@
+/*
+    API functions needed for retrieving steam info and hours
+
+    Steam API Key: C07812B75A464A1431BB501AE01D2BB8
+
+*/
+
+const request = require('request');
+const axios = require('axios');
+
+async function getSteamJSON(URL) {
+    try {
+      const response = await axios.get("https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=C07812B75A464A1431BB501AE01D2BB8&steamid=76561197960434622&format=json");
+      console.log(JSON.stringify(response.data));
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+ function returnSteamInfo(steamID) {
+    const API = "C07812B75A464A1431BB501AE01D2BB8";
+    const format = "json"
+    let URL = `"https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=C07812B75A464A1431BB501AE01D2BB8&steamid=${steamID}&format=json"`;
+    let userInfo = getSteamJSON(URL);
+    return userInfo;
+    //let hours = 0;
+    //return hours;
+}
+
+
+function grabHours(steamID) {
+    info = returnSteamInfo(steamID);
+    // grab hours somehow;
+}
 
 
 export const grabID = function(req, res) {
-    console.log(req.body);
-    return "";
+    console.log(req.body.SteamID);
+    let SteamID = req.body.SteamID;
+    let hours = returnSteamInfo(SteamID);
+    res.status(200).send(`${hours}`);
 }
