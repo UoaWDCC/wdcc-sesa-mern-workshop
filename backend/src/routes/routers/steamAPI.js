b337fb59ec301f35d15e3ef05e8ddbe126649804
JDCRecomendable <JDCRecomendable@protonmail.ch>
2021-07-31 19:16:18 +1200

    small todolist

--- backend/src/routes/routers/steamAPI.js
+++ backend/src/routes/routers/steamAPI.js
@@ -2,6 +2,13 @@ import express from 'express';
 import { grabID } from '../api/steamapi';
 
 
+/*
+    Check if in game
+    retrieve pfp
+    user creation
+    find game request
+*/
+
 
 const router = express.Router();
 
